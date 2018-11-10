abe2c623e244685fda1f5f1d34ccb12a17865019
Kevin Michael Frick <kmfrick98@gmail.com>
2018-11-10 10:44:18 +0100

    Updates

---
+++ precache-manifest.9c7edd51a2f3a887cb4c2cbd4e1e66b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e23cf36892282bef59c4",
+    "url": "/orario-sync_react/static/js/runtime~main.e23cf368.js"
+  },
+  {
+    "revision": "d822edf280e970a31c05",
+    "url": "/orario-sync_react/static/js/main.d822edf2.chunk.js"
+  },
+  {
+    "revision": "35f71671ba6c63156a7d",
+    "url": "/orario-sync_react/static/js/1.35f71671.chunk.js"
+  },
+  {
+    "revision": "d822edf280e970a31c05",
+    "url": "/orario-sync_react/static/css/main.61342775.chunk.css"
+  },
+  {
+    "revision": "da84477b1950092e6cd5540898afdef0",
+    "url": "/orario-sync_react/index.html"
+  }
+];
