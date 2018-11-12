b2760d14706db1bed237457c99b5821ef443c585
Kevin Michael Frick <kmfrick98@gmail.com>
2018-11-12 17:20:09 +0100

    Updates

--- precache-manifest.a845ba574882eff2f68fe178621cfaef.js
+++ precache-manifest.a845ba574882eff2f68fe178621cfaef.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/orario-sync_react/static/js/runtime~main.e23cf368.js"
   },
   {
-    "revision": "98597bc9fc5acc48481f",
-    "url": "/orario-sync_react/static/js/main.98597bc9.chunk.js"
+    "revision": "1c855a1f904fb1d0692d",
+    "url": "/orario-sync_react/static/js/main.1c855a1f.chunk.js"
   },
   {
     "revision": "35f71671ba6c63156a7d",
     "url": "/orario-sync_react/static/js/1.35f71671.chunk.js"
   },
   {
-    "revision": "98597bc9fc5acc48481f",
+    "revision": "1c855a1f904fb1d0692d",
     "url": "/orario-sync_react/static/css/main.61342775.chunk.css"
   },
   {
-    "revision": "173d30d06e3cb7f9c2911e2973c0c530",
+    "revision": "815c96d58fade4f565ae5d7f3f9942b3",
     "url": "/orario-sync_react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/orario-sync_react/precache-manifest.5ceae3a2557600adf52e8de1c33a4287.js"
+  "/orario-sync_react/precache-manifest.a845ba574882eff2f68fe178621cfaef.js"
 );
 
 workbox.clientsClaim();
