4500769a618954d81e23ed9f683a0de32073aaf8
Kevin Michael Frick <kmfrick@proton.me>
2026-02-12 19:31:09 +0100

    Updates

---
+++ _next/static/4sj4IDKNeg3tMT_3d7958/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/a697e83cc42c2005.js"
+  ],
+  "/_error": [
+    "static/chunks/377f7526d8496122.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/a697e83cc42c2005.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/b3c7f4336df06aec.js",
+  "static/chunks/c169041c3f5fa888.js",
+  "static/chunks/800cb3e4ca8de1db.js",
+  "static/chunks/turbopack-bf89b3b42b1a135b.js"
+])
